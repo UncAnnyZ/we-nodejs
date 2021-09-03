1d2ddfa78ad890a855b00597e3039be2e4442fd1
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2021-09-03 22:44:09 +0800

    修复记录
    修复记录

--- miniprogram/pages/achievement/achievement.js
+++ miniprogram/pages/achievement/achievement.js
@@ -15,63 +15,8 @@ Page({
 
   },
   bindPickerChange(e) {
-<<<<<<< HEAD
     console.log(e)
     this.showList(e.detail.value)
-=======
-
-    var a = 0,
-      b = 0,
-      c = 0,
-      d = 0,
-      k = 0,
-      f = 0;
-    var isCourse = 'none';
-    let data = [],
-      color = [],
-      achievement = this.data.achievement;
-      
-    for (var i = 0; i < achievement.length; i++) {
-      if (achievement[i].xnxqmc == this.data.array[e.detail.value]) {
-        data.push(achievement[i]);
-        a = Number(achievement[i].xf) + a;
-        b = Number(achievement[i].cjjd) + b;
-        c = c + Number(achievement[i].xf) * Number(achievement[i].cjjd);
-        if (achievement[i].kcdlmc != "公共选修课") {
-          d = Number(achievement[i].xf) + d;
-          k = Number(achievement[i].cjjd) + k;
-          f = Number(achievement[i].xf) * Number(achievement[i].cjjd) + f;
-        }
-      }
-    }
-    if (data[0] == undefined) {
-      isCourse = '';
-    }
-    var pj_credit = c / a;
-    var pj_credit2 = f / d;
-    for (let i = 0; i < data.length; i++) {
-      if (parseInt(data[i].zcj) >= 90) {
-        color.push("#11c1f3");
-      } else if (parseInt(data[i].zcj) >= 80) {
-        color.push("#33cd5f");
-      } else if (parseInt(data[i].zcj) >= 70) {
-        color.push("#886aea");
-      } else if (parseInt(data[i].zcj) >= 60) {
-        color.push("#ffc900");
-      } else {
-        color.push("#CC0000");
-      }
-    }
-    this.setData({
-      isCourse: isCourse,
-      index: e.detail.value,
-      list: data,
-      color: color,
-      TotalCredit: a,
-      AverageScorePoint: pj_credit.toFixed(2),
-      Achi: pj_credit2.toFixed(2)
-    })
->>>>>>> 234bccad06c0e20fa51165e65438b54490ff4640
   },
   onLoad: function () {
     getApp().loginState();
