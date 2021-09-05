433269ef9909b3ffc1bfc5c39e75ae7c077eeca8
li1055107552 <li1055107552@163.com>
2021-09-05 20:09:12 +0800

    优化日历

--- miniprogram/pages/calendar/calendar.js
+++ miniprogram/pages/calendar/calendar.js
@@ -13,45 +13,45 @@ Page({
       dayOfWeek: "星期" + util.formatDay(new Date().getDay()),
     },
     isLoading: true,
-    index:"0",
+    index: "0",
     startX: 0, //开始坐标
     startY: 0,
   },
-  
-  feedbackHandler(e) {                                                //跳转到子页
+
+  feedbackHandler(e) { //跳转到子页
     wx.navigateTo({
       url: 'addition/addition'
     })
-  }, 
+  },
 
-  
-  num_data: function (start_date1,end_date1) {                        //计算倒数日
+
+  num_data: function (start_date1, end_date1) { //计算倒数日
     var start_date = new Date(start_date1.replace(/-/g, "/"));
     var end_date = new Date(end_date1.replace(/-/g, "/"));
     var days = end_date.getTime() - start_date.getTime();
     var day = parseInt(days / (1000 * 60 * 60 * 24));
-    return day*-1;
+    return day * -1;
   },
 
-  terms:function(){                                                    //学年显示
-    var year='';
-    if(new Date().getMonth()>4){
-      year=new Date().getFullYear() + '-' + (new Date().getFullYear()+1) + '-' + 1
-    }else{
-      year=new Date().getFullYear()-1 + '-' + new Date().getFullYear() + '-' + 2
+  terms: function () { //学年显示
+    var year = '';
+    if (new Date().getMonth() > 4) {
+      year = new Date().getFullYear() + '-' + (new Date().getFullYear() + 1) + '-' + 1
+    } else {
+      year = new Date().getFullYear() - 1 + '-' + new Date().getFullYear() + '-' + 2
     }
     this.setData({
-      term:year
+      term: year
     })
   },
-  
-  setDataCalendar: function() {                                       //页面渲染全部倒数日
+
+  setDataCalendar: function () { //页面渲染全部倒数日
     var addday = getApp().globalData._adday;
-    var xlist=[];
-    var nowdate=new Date().getFullYear() + '-' + (new Date().getMonth()+1) + '-' + new Date().getDate();
-    console.log("yesr",nowdate);
+    var xlist = [];
+    var nowdate = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
+    console.log("yesr", nowdate);
     for (let i = 0; i < addday.length; i++) {
-      var gapDays2=this.num_data(addday[i].holidayDate,nowdate);
+      var gapDays2 = this.num_data(addday[i].holidayDate, nowdate);
       xlist.push({
         holidayName: addday[i].holidayName,
         holidayDate: addday[i].holidayDate,
@@ -60,35 +60,41 @@ Page({
         isTouchMove: false
       })
     }
-    console.log("before-list",xlist)
+    console.log("before-list", xlist)
     this.setData({
       show: "",
       list: xlist
     })
   },
-  
-  touchstart: function (e) {                                            //开始触摸时 重置所有删除
+
+  touchstart: function (e) { //开始触摸时 重置所有删除
     this.data.list.forEach(function (v, i) {
-      if (v.isTouchMove)//只操作为true的
-      v.isTouchMove = false;
+      if (v.isTouchMove) //只操作为true的
+        v.isTouchMove = false;
     })
-    
+
     this.setData({
       startX: e.changedTouches[0].clientX,
       startY: e.changedTouches[0].clientY,
-      list:this.data.list
+      list: this.data.list
     })
   },
 
   touchmove: function (e) {
     var that = this,
-    index = e.currentTarget.dataset.index,//当前索引
-    startX = that.data.startX,//开始X坐标
-    startY = that.data.startY,//开始Y坐标
-    touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
-    touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
-    //获取滑动角度
-    angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+      index = e.currentTarget.dataset.index, //当前索引
+      startX = that.data.startX, //开始X坐标
+      startY = that.data.startY, //开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX, //滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY, //滑动变化坐标
+      //获取滑动角度
+      angle = that.angle({
+        X: startX,
+        Y: startY
+      }, {
+        X: touchMoveX,
+        Y: touchMoveY
+      });
     that.data.list.forEach(function (v, i) {
       v.isTouchMove = false
       //滑动超过30度角 return
@@ -100,96 +106,96 @@ Page({
           v.isTouchMove = true
       }
     })
-    
+
     //更新数据
     that.setData({
-    list: that.data.list
+      list: that.data.list
     })
   },
 
-    angle: function (start, end) {
-      var _X = end.X - start.X,
+  angle: function (start, end) {
+    var _X = end.X - start.X,
       _Y = end.Y - start.Y
-      //返回角度 /Math.atan()返回数字的反正切值
-      return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-    },
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
 
-    del: function (e) {                                                 //删除倒数日  
-      this.data.list.splice(e.currentTarget.dataset.index, 1)
-      wx.cloud.callFunction({
-        name: 'we_adday1',
-        data: {
-          _adday: JSON.stringify(this.data.list),
-          username: getApp().globalData.username,
-        },
-        success: res => {
-          wx.showToast({
-            title: '删除成功',
-            icon: 'none',
-          })
-          this.setData({
-            list: this.data.list
-          })
-        },
-        fail: err => {
-          wx.showToast({
-            title: '删除失败',
-            icon: 'none',
-          })
-        }
-      })
-    },
-     
+  del: function (e) { //删除倒数日  
+    this.data.list.splice(e.currentTarget.dataset.index, 1)
+    wx.cloud.callFunction({
+      name: 'we_adday1',
+      data: {
+        _adday: JSON.stringify(this.data.list),
+        username: getApp().globalData.username,
+      },
+      success: res => {
+        wx.showToast({
+          title: '删除成功',
+          icon: 'none',
+        })
+        this.setData({
+          list: this.data.list
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          title: '删除失败',
+          icon: 'none',
+        })
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {                                           //读取数据库
+  onLoad: function (options) { //读取数据库
     this.terms();
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onShow: function(options) {
+  onShow: function (options) {
     wx.cloud.callFunction({
-      name:'readday',
-      data:{
+      name: 'readday',
+      data: {
         username: getApp().globalData.username,
         type: 'read'
       },
-      success:res=>{
+      success: res => {
         console.log(res, 1111)
-        getApp().globalData._adday=JSON.parse(res.result)
+        getApp().globalData._adday = JSON.parse(res.result)
         this.setDataCalendar();
       },
-      fail:err=>{
+      fail: err => {
         console.log(err)
       }
     })
     // 调用函数时，传入new Date()参数，返回值是日期和时间  
-  
+
   },
 
 
-  onReady: function() {
+  onReady: function () {
 
   },
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function(res) {
+  onShareAppMessage: function (res) {
     return {
       title: '广东石油化工学院校历',
       path: 'pages/calendar/calendar',
     }
   },
-  showPic: function() {
+  showPic: function () {
     wx.previewImage({
       current: 'cloud://un1-d62c68.756e-un1-d62c68-1258307938/xl.png', // 当前显示图片的http链接
       urls: ['cloud://un1-d62c68.756e-un1-d62c68-1258307938/xl.png'] // 需要预览的图片http链接列表
