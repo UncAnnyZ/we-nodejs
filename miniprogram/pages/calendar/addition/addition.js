35b6428557ed8bdd4a5405202d2ae98c181ae233
ChanWingKei233 <89628135+ChanWingKei233@users.noreply.github.com>
2021-09-13 21:45:51 +0800

    优化4.0小细节~~~

--- miniprogram/pages/calendar/addition/addition.js
+++ miniprogram/pages/calendar/addition/addition.js
@@ -3,7 +3,11 @@ Page({
   data: {
     isLoading: true,
   },
-  
+  tapPrev:function(){
+    wx.navigateBack({//返回
+      delta: 1
+    })
+  },
   onLoad:function(options){                     //接收url传递的数据并渲染
     this.setData({
       Name:options.Name,
